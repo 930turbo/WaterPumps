1017efbbd2ab5d75fb0c87a1a2fc1ab81da0e5f8
Gιαииι <35586763+dieslowly@users.noreply.github.com>
2021-02-17 10:18:24 -0500

    Update waterpumps.py

--- waterpumps.py
+++ waterpumps.py
@@ -1,12 +1,23 @@
 # import statements
 import pandas as pd
+import os
+import glob
 
-# generate data frames to view data
+# using glob we match the pattern name 'csv' to save the list of file names as the variable 'csvfile'
+extension = 'csv'
+csvfile = [i for i in glob.glob('*.{}'.format(extension))]
+
+# concatenate all files in given list and export as a single csv file
+combined_csv = pd.concat([pd.read_csv(f) for f in csvfile ])
+# now export to csv
+combined_csv.to_csv( "combineddata.csv", index=False, encoding='utf-8-sig')
+
+# generate data frames to view data in terminal
 df = pd.read_csv ('testset.csv')
 df2 = pd.read_csv ('trainsetlabels.csv')
 df3 = pd.read_csv ('trainsetvalues.csv')
 
-# print out data frames
+# print out data frames in terminal
 print(df)
 print(df2)
 print(df3)
