6b6f99fce97ef8c89c043b1c62ab3fb07b06070c
Gιαииι <35586763+dieslowly@users.noreply.github.com>
2021-02-17 10:13:15 -0500

    Update waterpumps.py

--- waterpumps.py
+++ waterpumps.py
@@ -1,9 +1,12 @@
+# import statements
 import pandas as pd
 
+# generate data frames to view data
 df = pd.read_csv ('testset.csv')
 df2 = pd.read_csv ('trainsetlabels.csv')
 df3 = pd.read_csv ('trainsetvalues.csv')
 
+# print out data frames
 print(df)
 print(df2)
 print(df3)
